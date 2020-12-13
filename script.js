d91fd00b1ebb5c531abf5929a776ca818b3d09b1
Fujinjakka <calvieri.marco@gmail.com>
2020-12-13 19:27:31 +0100

    Struttura complete

---
+++ script.js
@@ -0,0 +1,100 @@
+var app = new Vue(
+  {
+  el: "#vue_container",
+    data: {
+      contacts: [
+      	{
+      		name: 'Ironman',
+      		avatar: 'img/ironman.png',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Hai portato a spasso il cane?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Ricordati di dargli da mangiare',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 16:15:22',
+      				text: 'Tutto fatto!',
+      				status: 'received'
+      			}
+      		],
+      	},
+      	{
+      		name: 'Spiderman',
+      		avatar: 'img/spiderman.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '20/03/2020 16:30:00',
+      				text: 'Ciao come stai?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '20/03/2020 16:30:55',
+      				text: 'Bene grazie! Stasera ci vediamo?',
+      				status: 'received'
+      			},
+      			{
+      				date: '20/03/2020 16:35:00',
+      				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+      				status: 'sent'
+      			}
+      		],
+      	},
+      	{
+      		name: 'Hulk',
+      		avatar: 'img/hulk.png',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '28/03/2020 10:10:40',
+      				text: 'La Marianna va in campagna',
+      				status: 'received'
+      			},
+      			{
+      				date: '28/03/2020 10:20:10',
+      				text: 'Sicuro di non aver sbagliato chat?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '28/03/2020 16:15:22',
+      				text: 'Ah scusa!',
+      				status: 'received'
+      			}
+      		],
+      	},
+      	{
+      		name: 'Captain America',
+      		avatar: 'img/captainamerica',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Lo sai che ha aperto una nuova pizzeria?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Si, ma preferirei andare al cinema',
+      				status: 'received'
+      			}
+      		],
+      	},]
+      },
+
+    methods: {
+      functionName: function () {
+        this.elementName.push(this.elementName);
+      },
+      functionName: function (item) {
+        this.elementName.splice(this.elementName.indexOf(item), 1);
+      }
+    }
+  }
+);
